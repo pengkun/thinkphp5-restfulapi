1a1bd9580d6a1234d4c817d671ad0945692c879b
pengkun <476361229@qq.com>
2021-09-02 17:45:23 +0800

    数据库，路由

--- route/route.php
+++ route/route.php
@@ -12,11 +12,11 @@
 use think\facade\Route;
 
 //一般路由规则，访问的url为：v1/address/1,对应的文件为Address类下的read方法
-Route::get(':version/address/:id', 'api/:version.user/address');
+Route::get(':v1/address/:id', 'api/:v1.user/address');
 
-Route::resource(':version/user', 'api/:version.user');       //资源路由，详情查看tp手册资源路由一章
+Route::resource(':v1/user', 'api/:v1.user');       //资源路由，详情查看tp手册资源路由一章
 
 //生成access_token，post访问Token类下的token方法
-Route::post(':version/token', 'api/:version.token/token');
-Route::post(':version/token/refresh', 'api/:version.token/refresh');
+Route::post(':v1/token', 'api/:v1.token/token');
+Route::post(':v1/token/refresh', 'api/:v1.token/refresh');
 
